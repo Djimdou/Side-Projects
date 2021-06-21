8766ac41f169eb166b58b23ce1bc8888fcdc1812
Magloire LOUDEGUI DJIMDOU <50971645+Djimdou@users.noreply.github.com>
2021-06-20 20:08:34 -0400

    Add files via upload

--- house_spending/ShinyAppCodes.R
+++ house_spending/ShinyAppCodes.R
@@ -1,6 +1,6 @@
 library(shiny)
 
-ui <- fluidPage("Household Spending in 2017",
+ui <- fluidPage("Household Spending",
                 selectInput(inputId="prov",
                             label="Select a province",
                             choices=c("14 - Atlantic provinces",
@@ -11,22 +11,33 @@ ui <- fluidPage("Household Spending in 2017",
                                       "48 - Alberta",
                                       "59 - British Columbia",
                                       "63 - Territorial capitals")
-                              #sort(unique(diary$Prov))
                             ),
-                plotOutput(outputId="pie",
-                           pie(diary[diary$Prov==14,'HHType6']))
+                plotOutput(outputId="pie")
                 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  
+  # # Data: Enquête sur les dépenses des ménages, at 
+  # https://www150.statcan.gc.ca/n1/pub/62m0004x/62m0004x2017001-fra.htm
+  
+  
+  #setwd("C:/Users/djimd/Downloads/SHS_EDM_2017-fra/")
+  diary <- read.csv(file="C:/Users/djimd/Downloads/SHS_EDM_2017-fra/SHS_EDM_2017/Data - Données/CSV/SHS-62M004X-E-2017-Diary_F1.csv",
+                    header=TRUE)
+  output$pie <- renderPlot({
+    title <- "Household type"
+    pie(table(factor(diary[diary$Prov==as.integer(substr(input$prov,1,2)),'HHType6'],
+                     levels = 1:6,
+                     labels=c("One person household",
+                              "Couple without children",
+                              "Couple with children",
+                              "Couple with other related or unrelated persons",
+                              "Lone parent family with no additional persons",
+                              "Other household with related or unrelated persons")))
+    )
+  })
+}
 
 shinyApp(ui = ui, server = server)
 
-# # Data: EnquÃªte sur les dÃ©penses des mÃ©nages, at 
-# https://www150.statcan.gc.ca/n1/pub/62m0004x/62m0004x2017001-fra.htm
 
-
-#setwd("C:/Users/djimd/Downloads/SHS_EDM_2017-fra/")
-
-diary <- read.csv(file="C:/Users/djimd/Downloads/SHS_EDM_2017-fra/SHS_EDM_2017/Data - DonnÃ©es/CSV/SHS-62M004X-E-2017-Diary_F1.csv",
-                  header=TRUE)
-  
